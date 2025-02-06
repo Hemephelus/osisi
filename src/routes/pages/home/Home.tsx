49ea0e264b819d379b00e16eafcb5a456396ca07
Nwachukwu Ujubuonu <nwachukwuujubuonu@gmail.com>
2025-02-06 17:03:22 +0100

    edited home page

--- src/routes/pages/home/Home.tsx
+++ src/routes/pages/home/Home.tsx
@@ -5,7 +5,7 @@ function Home() {
     <main className="flex flex-col justify-center items-center text-center min-h-screen px-[20%] gap-12 font-sora">
       <section className="flex flex-col justify-center items-center">
       <h3 className="text-6xl font-overlock-sc">Welcome to Osisi</h3>
-      <h5 className="text-3xl">Remember your roots</h5>
+      <h5 className="text-3xl">Remembering our roots</h5>
       </section>
       <Link to={'/family'} className="text-lg  bg-sec text-pri p-4 hover:scale-95 duration-300 ">
         View Our Family
